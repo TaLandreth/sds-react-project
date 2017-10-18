ab8e6e8642eb0c2f978d48f3d2c708dc065343d3
Tanya Landreth <tanya@smartdatasystems.net>
2017-10-18 09:21:00 -0400

    infinite scroll start

---
+++ src/_oldstuff.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+                            {/*                     
+                            {this.props.page ? this.props.page.map((b) =>
+                        <div key={b.id} className="tile">
+                            <div key={b.author} className="middletile">{b.author}</div>
+                            <div key={b.title} className="middletile">{b.title}</div>
+                            <div key={b.genre} className="middletile">{b.genre}</div>
+                            <div key={b.year} className="middletile">{b.year}</div>
+
+                            <div className="actions" className="righttile"><button
+                                onClick={this.editing.bind(this, b)}>Edit</button></div>
+                            <div className="actions" className="righttile"><button
+                                onClick={this.deleteBook.bind(this, b)}>Delete</button></div>
+                        </div>) : null
+                    } */}
+
+
+
+/*     view(criteria) {
+                        console.log("in Books change view")
+        console.log(criteria)
+
+        getPagedBooks(this.props.dispatch, criteria) //how many to view, where to start from
+    }
+
+    sort(num, sortInst) {
+                        console.log("Sort Instructions:")
+        console.log(sortInst)
+
+        sortBy(this.props.dispatch, num, sortInst)
+    } 
+       addData(newBook) {
+                        addABook(this.props.dispatch, newBook)
+                    }//end add 
+
+    deleteData(book) {
+        deleteBook(this.props.dispatch, book)
+    }//end delete 
+
+    editData(book) {
+        editBook(this.props.dispatch, book)
+    }//end edit 
+
+   searchData(data) {
+        console.log("Sending to dispatcher:")
+        console.log(data)
+        searchFor(this.props.dispatch, data)
+    }
+
+    paging(queryCriteria) {
+        console.log("Query Criteria:")
+        console.log(queryCriteria)
+
+        //sending to pagePages in dispatcher the current starting record #
+        getPagedBooks(this.props.dispatch, queryCriteria)
+    } 
+    */
